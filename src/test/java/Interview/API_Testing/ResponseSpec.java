b902aa6ee33c2a024ad9e4dfe5fd9250438d4085
Srikar <srikar511@outlook.com>
2025-09-10 22:01:07 +0530

    Commit on 10th Sept_

---
+++ src/test/java/Interview/API_Testing/ResponseSpec.java
@@ -0,0 +1,18 @@
+package Interview.API_Testing;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.ResponseSpecification;
+
+import static org.hamcrest.Matchers.*;
+
+public class ResponseSpec {
+
+    public static void main(String[] args) {
+
+        ResponseSpecification resspec = (ResponseSpecification) new ResponseSpecBuilder().expectContentType("Application/json")
+                .expectStatusCode(200).expectBody("ststud",equalTo("mawa"));
+
+
+
+    }
+}
