9b6fbbc49cafa492ecc706fd719328010d1d3b29
Srikar <Srikarmsd@gmail.com>
2025-11-25 14:44:46 +0530

    Commit on 25th Nov___

---
+++ src/test/java/Interview/Algorithms/Rotate_Array.java
@@ -0,0 +1,13 @@
+package Interview.Algorithms;
+
+public class Rotate_Array {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] rotete(int[] arr){
+        return arr;
+    }
+}
+
